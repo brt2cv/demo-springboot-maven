1207f5b1a281ea7c066303820ff285c809348f8e
Bright@huawei <brt2@qq.com>
2022-01-20 16:57:34 +0800

    提供svn命令用于编译运行

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,17 @@
+// 命令行编译运行: mvn clean compile && mvn exec:java -Dexec.mainClass='com.example.demo.DemoApplication'
 package com.example.demo;
+
+/*
+public class HelloApplication {
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloApplication().sayHello());
+    }
+}
+*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +22,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
